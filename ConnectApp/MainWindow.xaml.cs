02f9fcb58e6a4b4ed025c51827ce7638bb1a8732
Poroh <meta.poroh.kun@gmail.com>
2018-12-11 01:51:10 +0300

    serial port pc connecting

---
+++ ConnectApp/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ConnectApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string[] PortsList { get; } = new string[] { "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9", "COM10", "COM11", "COM12" };
+        public string SelectedPort { get; set; } = "COM3";
+        public string ConnectStatus => _status.ToString();
+        public bool IsConnected => _status == ConnectionStatus.Connected;
+        public bool IsDisconnected => _status == ConnectionStatus.Disconnected;
+        public string LogString { get; private set; }
+
+        private ConnectionStatus _status = ConnectionStatus.Disconnected;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private AdvancedSerialClient _serialClient;
+
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            _serialClient = new AdvancedSerialClient();
+
+            _serialClient.Open(SelectedPort, 115200);
+
+            var message = new SerialProtocol.AdvancedSerialMessage();
+            message.ID = 0;
+            _serialClient.Send(message);
+        }
+
+        private void DisconnectButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- DoorLock.ino
+++ DoorLock.ino
@@ -1,13 +1,16 @@
 /*
-    Name:       DoorLock.ino
-    Created:	11.12.2018 0:34:21
-    Author:     POROH-PC\P
+	Name:       DoorLock.ino
+	Created:	11.12.2018 0:34:21
+	Author:     POROH-PC\P
 */
 
+#define BACKLIGHT_ON_MESSAGE 0
+#define BACKLIGHT_OFF_MESSAGE 1
 
 //#include <EEPROM.h>
 #include "Toggle.h"
 #include "Button.h"
+#include "AdvancedSerial.h"
 
 Button btn1(2, btnClick);
 Button btn2(3, btnClick);
@@ -21,13 +24,29 @@ bool led = false;
 void setup()
 {
 	pinMode(ledPin, OUTPUT);
-	//Serial.begin(115200);
+	Serial.begin(115200);
+	//configure receiver callback
+	AdvancedSerial.setReceiver(onMessage);
 }
 
 void btnClick(Button* sender)
 {
 	//Serial.println(count, DEC);
 	led = !led;
+}
+
+void onMessage(AdvancedSerialMessage* message)
+{
+	switch (message->id)
+	{
+	case BACKLIGHT_ON_MESSAGE:
+		digitalWrite(ledPin, HIGH);
+		break;
+
+	case BACKLIGHT_OFF_MESSAGE:
+		digitalWrite(ledPin, LOW);
+		break;
+	}
 }
 
 void loop()
@@ -37,7 +56,8 @@ void loop()
 	btn3.Read();
 	btn4.Read();
 	toggle.Read();
+	AdvancedSerial.loop();
 
-	digitalWrite(ledPin, led ? HIGH : LOW);
-	digitalWrite(ledPin, toggle.IsOn() ? HIGH : LOW);
+	/*digitalWrite(ledPin, led ? HIGH : LOW);
+	digitalWrite(ledPin, toggle.IsOn() ? HIGH : LOW);*/
 }
